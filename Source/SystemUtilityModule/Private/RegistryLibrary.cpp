d27236994acb85ea6fa19a95994a8418db6a8aa9
AldertLake <dihafkrk@anam9wd.co>
2025-07-03 18:37:52 +0100

    Adding New Class

---
+++ Source/SystemUtilityModule/Private/RegistryLibrary.cpp
@@ -0,0 +1,5 @@
+// ---------------------------------------------------
+// Copyright (c) 2025 AldertLake. All Rights Reserved.
+// GitHub:   https://github.com/AldertLake/
+// Support:  https://ko-fi.com/aldertlake
+// ---------------------------------------------------
